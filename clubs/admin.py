7baf2dc65539a04c844e6cf3743b4ef00fbccffc
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2023-06-06 16:53:54 -0400

    Set up models for users clubs, and messages (no images)

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Club, Message
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "username", "isAdmin")
+
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "interestCount", "creator")
+    filter_horizontal = ("editors", "messages")
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "poster", "timestamp")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Club, ClubAdmin)
+admin.site.register(Message, MessageAdmin)
--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser): # Username is used for login and message board, email is also used for other info
+    isAdmin = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"USER{self.id} - {self.email} - {self.username}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "isAdmin": self.isAdmin,
+        }
+
+class Club(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    # Image Field
+    announcement = models.TextField(blank=True)
+    interestCount = models.IntegerField(default=0)
+    editors = models.ManyToManyField("User", related_name="clubsEditing")
+    creator = models.ForeignKey("User", on_delete=models.PROTECT, related_name="clubsCreated")
+    messages = models.ManyToManyField("Message", related_name="clubs")
+
+    def __str__(self):
+        return f"CLUB{self.id} - {self.title}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            # Image Field
+            "announcement": self.announcement,
+            "interestCount": self.interestCount,
+            "editors": [user.email for user in self.editors.all()],
+            "creator": self.creator.email,
+            "messages": [message.id for message in self.messages.all()],
+        }
+
+class Message(models.Model):
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    poster = models.ForeignKey("User", on_delete=models.CASCADE, related_name="messagesPosted")
+
+    def __str__(self):
+        return f"MESSAGE{self.id} - By: {self.poster}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I: %M %p"),
+            "poster": self.poster.username,
+        }
