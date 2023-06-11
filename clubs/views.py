1eb88f881d4591c20ad7efc89aaca32fc5aa23ba
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2023-06-10 20:44:58 -0400

    files and navbar

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "clubs/index.html")
+
+def login_view(request):
+    return render(request, "clubs/login.html")
+
+def logout_view(request):
+    return render(request, "clubs/login.html")
+
+def register(request):
+    return render(request, "clubs/register.html")
