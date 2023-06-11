907e31732b3ba0d00396c97b9cccf4e6ec91f05f
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2023-06-10 21:04:38 -0400

    login and registration

--- clubs/views.py
+++ clubs/views.py
@@ -1,4 +1,15 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+
+from .models import User, Club, Message
 
 # Create your views here.
 
@@ -6,10 +17,46 @@ def index(request):
     return render(request, "clubs/index.html")
 
 def login_view(request):
-    return render(request, "clubs/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "clubs/login.html", {
+                "message": "Invalid username and/or password given."
+            })
+    else:
+        return render(request, "clubs/login.html")
 
 def logout_view(request):
-    return render(request, "clubs/login.html")
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
 
 def register(request):
-    return render(request, "clubs/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "clubs/register.html", {
+                "message": "The password and confirmation must match."
+            })
+        
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "clubs/register.html", {
+                "message": "That username is already taken."
+            })
+        
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "clubs/register.html")
