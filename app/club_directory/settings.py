a778d816d10d4b0708a80121629f3d09f122ca8b
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2023-08-04 19:32:38 -0400

    static and media files, with same localhost issue plus csrf trusted origins issue

--- app/club_directory/settings.py
+++ app/club_directory/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -130,4 +131,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/usr/src/app/media/'
+MEDIA_ROOT = BASE_DIR / "mediafiles"
